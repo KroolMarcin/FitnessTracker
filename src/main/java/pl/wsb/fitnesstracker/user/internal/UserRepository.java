a71bd8f23239678cab8e867cd86b13993ddbe83c
KroolMarcin <krolmarcinn@gmail.com>
2025-01-10 23:56:39 +0100

    Fix

---
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserRepository.java
@@ -0,0 +1,11 @@
+
+package pl.wsb.fitnesstracker.user.internal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
